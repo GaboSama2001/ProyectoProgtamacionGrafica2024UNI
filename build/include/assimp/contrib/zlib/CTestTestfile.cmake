aa7ee99cc78b6d97909841b65df50b79defa56c0
GaboSama2001 <ernestogabrielsanchezespinosa@gmail.com>
2024-07-04 23:51:34 -0600

    Ejecucion

---
+++ build/include/assimp/contrib/zlib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/MSI GF65/source/repos/ProyectoProgtamacionGrafica2024UNI/include/assimp/contrib/zlib
+# Build directory: C:/Users/MSI GF65/source/repos/ProyectoProgtamacionGrafica2024UNI/build/include/assimp/contrib/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
